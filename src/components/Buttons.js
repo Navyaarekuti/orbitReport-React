7f70b682865f91ccdc6b62b61f0b53798fdd77e4
Navyaarekuti <arekutinavya1@gmail.com>
2023-07-20 15:02:15 -0500

    All files at a time

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,28 @@
-const Buttons = () => {
+import satData from './satData';
+
+// const Buttons = () => {
+//   return (
+//     <div>
+//       <button>Placeholder Button</button>
+//       <button>All Orbits</button>
+//       </div>
+//   );
+// };
+import "./styling.css";
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className="flex-container">
+     {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+
+      
+    </div>
   );
 };
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,54 @@
+// const Table = ({ sat }) => {
+//   return (
+//       <table>
+//        <thead>
+//         <tr>
+//           <th>Header TBD</th>
+//         </tr>
+//         </thead>
+//         <tbody>
+//         <tr>
+//           <td>Row Data TBD</td>
+//         </tr>
+//         </tbody>
+//       </table>
+//   );
+// };
+
+
+
+
 const Table = ({ sat }) => {
   return (
-      <table>
-       <thead>
+    <table>
+      <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
-      </table>
+      </thead>
+      <tbody>
+        {sat.map((data) => {
+          let status;
+          if (data.operational) {
+            status = 'Active';
+          } else {
+            status = 'Inactive';
+          }
+
+          return (
+            <tr key={data.id}>
+              <td>{data.name}</td>
+              <td>{data.type}</td>
+              <td>{data.launchDate}</td>
+              <td>{status}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
